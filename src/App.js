ab75a92cb733da85ac7b3118e1879f6b8c5ba6c6
gaellebauvin <gaelle_bauvin@ens.univ-artois.fr>
2020-04-27 15:12:48 +0200

    tes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Timer.js';
 
 
 function App() {
@@ -8,17 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Modifier <code>src/App.js</code> et enregistrer pour recharger.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Apprendre React
-        </a>
       </header>
     </div>
   );
