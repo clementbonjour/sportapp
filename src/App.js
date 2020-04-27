62eb8fc434afcd541921ebad162c6764cca7c38b
lucas lacroix <lacroixlucas59@gmail.com>
2020-04-27 16:28:49 +0200

    corps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
+import persona from './corps.svg';
+
 import './App.css';
 import './Timer.js';
 
@@ -10,6 +12,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div className="margium">
+        <img src={persona} className="persona" alt="persona" />
+      </div>
     </div>
   );
 }
