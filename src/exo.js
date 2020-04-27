a18dd95c4e6af6e28956adb90b557298849fb826
yoyooy <yoyooy>
2020-04-27 14:17:52 +0200

    exo

---
+++ src/exo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function lam() {
+  return (
+    <div className="exos-container">
+      <div className="exo-superior">yo</div>
+      <div className="exo-inferior">uo</div>
+    </div>
+  )
+}
+
+export default App;
