4febd7a38706e346f48d2626159d6cede85be79a
yoyooy <yoyooy>
2020-04-29 16:29:13 +0200

    INCHALLAH zolero

--- src/Exo.js
+++ src/Exo.js
@@ -38,10 +38,6 @@ if(exercices[3].id == 1){
                 <p>{exo.description}</p>
               </div>
             ))}
-          <div class="exo-detailTxt">
-            <h3>Wesh</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra, neque ac ultricies blandit, lacus ligula mattis purus, et ultricies est erat sit amet tortor. Vestibulum vel feugiat justo.</p>
-          </div>
           <div className="PlayPauseButton">
             <img src={Play}/>
           </div>
